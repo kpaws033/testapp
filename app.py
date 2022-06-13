ed806a1dda799da5d20e061614ffcc43795385a9
kpaws033 <purnaaws033@gmail.com>
2022-06-13 05:44:12 +0000

    port update

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import os
 from flask import Flask, session, url_for, redirect, render_template, request, abort, flash
 
 app = Flask(__name__)
-BACKEND_URL = os.environ.get("BACKEND_URL", default="http://localhost:8086")
+BACKEND_URL = os.environ.get("BACKEND_URL", default="http://127.0.0.1:5000")
 
 @app.errorhandler(401)
 def FUN_401(error):
