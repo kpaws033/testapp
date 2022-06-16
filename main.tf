c96bd34e7b28ecb4cd9faa692d35f3a2d96a6b78
pur kot <purkot@gmail.com>
2022-06-16 16:15:04 +0100

    update

--- main.tf
+++ main.tf
@@ -7,8 +7,8 @@ locals {
 }
 
 provider "aws" {
-  access_key = "XX"
-  secret_key = "XX"
+  access_key = "XXXXXX"
+  secret_key = "XXXXx"
   region     = var.aws_region
   #if you are running from AWS ec2 linux instance please use bellow credentials section
   #shared_credentials_file = "$HOME/.aws/credentials"
